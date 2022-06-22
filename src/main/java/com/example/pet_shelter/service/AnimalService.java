7a36227097b73b208cd154e5444a479a30298fec
Dmitry Kolomeets <bde@ukr.net>
2022-06-22 22:26:24 +0300

    added Animal entity

---
+++ src/main/java/com/example/pet_shelter/service/AnimalService.java
@@ -0,0 +1,21 @@
+package com.example.pet_shelter.service;
+
+import com.example.pet_shelter.entity.Animal;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+
+@Repository
+public interface AnimalService {
+
+    public List<Animal> getAllAnimals();
+
+    public  void saveAnimal(Animal animal);
+
+    public Animal getAnimal(int id);
+
+    public void deleteAnimal(int id);
+
+}
---
+++ src/main/resources/sql/insert animals.sql
@@ -0,0 +1,12 @@
+USE my_db;
+INSERT animals(breed, family, name) 
+VALUES
+('brangus', 'cow', 'Blacky'),
+('hereford', 'cow', 'Red'),
+('ongole', 'cow', 'Whity'),
+('poodle', 'dog', 'Nicy'),
+('сhihuahua', 'dog', 'Smily'),
+('lapdog', 'dog', 'Twicy'),
+('sphinx', 'cat', 'Hector'),
+('munchkin', 'cat', 'Luciy'),
+('ragdoll', 'cat', 'Kingmyau');
